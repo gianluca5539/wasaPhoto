365cc90f8a1e61f71ea7db8a800d5e9f7558e9b8
Gianluca Monte <gianluca.monte02@gmail.com>
2023-12-07 10:29:57 +0100

    removed jwt in favor of userid

--- service/api/login.go
+++ service/api/login.go
@@ -10,39 +10,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-type CustomJWTClaims struct {
-	UserID int `json:"userid"`
-	jwt.StandardClaims
-}
-
 type LoginRequest struct {
 	Username string `json:"username"`
-}
-
-const (
-	// JWTSigningKey is the key used to sign the JWT token
-	JWTSigningKey = "wasaphoto_secret" // in production should be put in .env or something
-)
-
-// Function to generate a JWT token
-func GenerateJWTToken(UserID int) (string, error) {
-	// Create the Claims
-	claims := CustomJWTClaims{
-		UserID,
-		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(), // Token will expire after 72 hours
-			Issuer:    "WASAPhoto",
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	ss, err := token.SignedString([]byte(JWTSigningKey))
-	if err != nil {
-		return "", fmt.Errorf("error in GenerateJWTToken while signing token: %w", err)
-	}
-
-	return ss, nil
 }
 
 
@@ -70,11 +39,7 @@ func (rt *_router) login(w http.ResponseWriter, r *http.Request, ps httprouter.P
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		} 
-		token, err := GenerateJWTToken(user.UserID)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+		token := user.UserID
 
 		res := map[string]interface{}{
 			"user": user,
@@ -85,12 +50,7 @@ func (rt *_router) login(w http.ResponseWriter, r *http.Request, ps httprouter.P
 		w.WriteHeader(http.StatusCreated)
 		json.NewEncoder(w).Encode(res)
 	} else {
-		token, err := GenerateJWTToken(user.UserID)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("internal server error" + err.Error()))
-			return
-		}
+		token := user.UserID
 
 		res := map[string]interface{}{
 			"user": user,
