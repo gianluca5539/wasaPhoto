883c5c9c97712153b697f60e2ca581864b44ae0a
Gianluca Monte <gianluca.monte02@gmail.com>
2023-12-17 18:00:14 +0100

    tx.Rollback() err handled

--- service/database/ban-user.go
+++ service/database/ban-user.go
@@ -13,35 +13,58 @@ func (db *appdbimpl) BanUser(id int, bannedBy int) error {
 	// Check if user is already banned and if not ban the user. Do everything in a transaction
 	tx, err := db.c.Begin()
 	if err != nil {
+		err2 := tx.Rollback()
+		if err2 != nil {
+			return err2
+		}
 		return err
 	}
-	defer tx.Rollback()
 
 	// Check if user is already banned
 	var exists bool
 	err = tx.QueryRow(check_ban_statement, id, bannedBy).Scan(&exists)
 	if err != nil {
+		err2 := tx.Rollback()
+		if err2 != nil {
+			return err2
+		}
 		return err
 	}
 
 	if exists {
+		err2 := tx.Rollback()
+		if err2 != nil {
+			return err2
+		}
 		return errors.New("user is already banned")
 	}
 
 	// Remove follow
 	_, err = tx.Exec(remove_follow_statement, bannedBy, id)
 	if err != nil {
+		err2 := tx.Rollback()
+		if err2 != nil {
+			return err2
+		}
 		return err
 	}
 
 	// Ban user
 	_, err = tx.Exec(ban_statement, id, bannedBy)
 	if err != nil {
+		err2 := tx.Rollback()
+		if err2 != nil {
+			return err2
+		}
 		return err
 	}
 
 	err = tx.Commit()
 	if err != nil {
+		err2 := tx.Rollback()
+		if err2 != nil {
+			return err2
+		}
 		return err
 	}
 
