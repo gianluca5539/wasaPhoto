63af2fc09cb1353cd33ddf21e986b4673c3a7844
Gianluca Monte <gianluca.monte02@gmail.com>
2023-12-22 10:37:30 +0100

    removed fmt

--- service/database/get-user-by-userid.go
+++ service/database/get-user-by-userid.go
@@ -3,7 +3,6 @@ package database
 import (
 	"database/sql"
 	"errors"
-	"fmt"
 
 	"github.com/gianluca5539/WASA/service/types"
 )
@@ -20,7 +19,6 @@ func (db *appdbimpl) GetUserByUserID(id int) (types.User, bool, error) {
 	err := db.c.QueryRow(query, id).Scan(&u.UserID, &u.Username, &nullableFeeling, &nullableBio, &nullablePicture)
 
 	if errors.Is(err,sql.ErrNoRows) {
-		fmt.Println("No rows were returned!")
 		return u, false, nil // not found but no error
 	}
 	if err != nil {
